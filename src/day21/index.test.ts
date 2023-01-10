9a44425f66d75e1ccf4706b4d6d2a3f46098f2bf
JohanS <johan.strom@mentimeter.com>
2023-01-10 21:10:39 +0100

    day21 part 2

--- src/day21/index.test.ts
+++ src/day21/index.test.ts
@@ -1,11 +1,78 @@
-import {  } from "./index";
+import { solve, arrangeExpression } from "./index";
 
 describe("index", () => {
-  describe("addCube", () => {
-    it("should add initial cube to scan", () => {
-      expect.assertions(1);
-      // const res = addCube([1, 1, 1]);
-      // expect(res.data.length).toBe(1);
+  describe("solve", () => {
+    it("should handle basic algebra", () => {
+      expect(solve("X + 1 = 2")).toBe(1);
+      expect(solve("1 + X = 2")).toBe(1);
+      expect(solve("X - 3 = 1 ")).toBe(4);
+      expect(solve("3 - X = 1 ")).toBe(2);
+      12;
+      expect(solve("2 * X = 4")).toBe(2);
+      expect(solve("X * 2 = 4")).toBe(2);
+      expect(solve("4 / X = 2")).toBe(2);
+      expect(solve("X / 4 = 2")).toBe(8);
+    });
+
+    it("should solve for X", () => {
+      // expect.assertions(1);
+      expect(solve("X = 1")).toBe(1);
+      expect(solve("(X) = 1")).toBe(1);
+      expect(solve("(X + 1) = 1")).toBe(0);
+      expect(solve("(1 + X) = 1")).toBe(0);
+      expect(solve("(X + 1) = 2")).toBe(1);
+      expect(solve("(1 + X) = 2")).toBe(1);
+      expect(solve("2 = (1 + X)")).toBe(1);
+      expect(solve("2 = (X + 1)")).toBe(1);
+      expect(solve("(X - 1) = 1")).toBe(2);
+      expect(solve("(2 * X) = 2")).toBe(1);
+      expect(solve("(2 / X) = 1")).toBe(2);
+      expect(solve("(2 * (X - 3)) = 2")).toBe(4);
+      expect(solve("(2 * (3 - X)) = 2")).toBe(2);
+      expect(solve("((3 - X) * 2) = 2")).toBe(2);
+      expect(solve("((3 / X) * 2) = 2")).toBe(3);
+      // Can't handle floats?
+      expect(solve("((3 * X) * 2) = 2")).toBe(1 / 3);
+      expect(solve("((4 + (2 * (X - 3))) / 4) = 150")).toBe(301);
+      expect(solve("((4 + (3 * (X - 3))) / 4) = 150") - 201.666).toBeLessThan(
+        0.1
+      );
+      expect(
+        solve("28 * (((49 + (X - 165)) / 2) + 1) = 1000") - 185.42857142857144
+      ).toBeLessThan(0.1);
+    });
+
+    it("should solve large problems", () => {
+      const expression =
+        "64 + ((819 + (2 * ((((2 * ((21 * (299 + (((((((((367 + ((2 * (((((224 + ((((((323 + (((6 * (((((560 + ((((116 + ((((((((((((((449 + (X - 165)) / 2) + 1) * 68) - 489) * 2) + 729) / 5) - 892) * 2) + 687) / 5) - 506) * 2)) / 2) - 71) / 2)) * 10) - 397) / 9) + 782)) - 249) / 3)) * 3) + 170) / 2) - 438) * 3)) / 4) - 160) * 4) + 249)) - 873)) / 12) + 667) + 490) / 3) - 983) * 5) - 900) / 5))) - 660)) + 495) / 3) - 230))) / 3) = 18688";
+      expect(solve(expression) - 1069).toBeLessThan(1);
+    });
+  });
+
+  describe("arrangeExpression", () => {
+    it("should push sort variables by algorithmic priority from left to right", () => {
+      // expect.assertions(1);
+      expect(arrangeExpression("(2 * (3 - X)) = 2")).toEqual([
+        "2 * (3 - X)",
+        "2",
+      ]);
+      expect(arrangeExpression("((3 - X) * 2) = 2")).toEqual([
+        "2 * (3 - X)",
+        "2",
+      ]);
+    });
+
+    it("Should handle large expression", () => {
+      const expression = `((21 * (299 + (((((((((367 + ((2 * (((((224 + ((((((323 + (((6 * (((((560 + ((((116 + ((((((((((((((449 + (X - 165)) / 2) + 1) * 68) - 489) * 2) + 729) / 5) - 892) * 2) + 687) / 5) - 506) * 2)) / 2) - 71) / 2)) * 10) - 397) / 9) + 782)) - 249) / 3)) * 3) + 170) / 2) - 438) * 3)) / 4) - 160) * 4) + 249)) - 873)) / 12) + 667) + 490) / 3) - 983) * 5) - 900) / 5))) - 660) = 10000`;
+      const expected = [
+        `-660 + (21 * (299 + (((((((((367 + ((2 * (((((224 + ((((((323 + (((6 * (((((560 + ((((116 + ((((((((((((((449 + (X - 165)) / 2) + 1) * 68) - 489) * 2) + 729) / 5) - 892) * 2) + 687) / 5) - 506) * 2)) / 2) - 71) / 2)) * 10) - 397) / 9) + 782)) - 249) / 3)) * 3) + 170) / 2) - 438) * 3)) / 4) - 160) * 4) + 249)) - 873)) / 12) + 667) + 490) / 3) - 983) * 5) - 900) / 5)))`,
+        "10000",
+      ];
+
+      const res = arrangeExpression(expression);
+
+      expect(res[1]).toBe(expected[1]);
+      expect(res[0]).toBe(expected[0]);
     });
   });
 });
--- src/day21/index.ts
+++ src/day21/index.ts
@@ -2,77 +2,199 @@ import { REAL_INPUT, TEST_INPUT } from "../utils/globals";
 import createLineProcessor from "../utils/lineProcessor";
 
 type Operation = string;
-type Instruction = Resolved | Unknown;
+type Expression = string;
+type Instruction = Resolved | Unknown | Pending;
 type Resolved = [id: string, n: number];
-type Unknown = [id: string, n: Operation];
+type Unknown = [id: string, operation: Operation];
+type Pending = [id: string, expression: Expression];
 
 const OPERAND = ["+", "-", "/", "*"] as const;
 type Operand = typeof OPERAND[number];
+
+const HUMAN = "humn" as const;
 
 async function getRootSum(filename = REAL_INPUT) {
   const input: Instruction[] = await readInput(filename);
 
   const parsed = input.map(([id, n]) => {
-    if (Number.isNaN(Number.parseInt(n as string))) {
+    if (id === HUMAN) {
+      return [id, "X"] as Pending;
+    } else if (Number.isNaN(Number.parseInt(n as string))) {
       return [id, n] as Unknown;
     } else {
-      return [id, parseInt(n as string)] as Resolved;
+      return [id, parseFloat(n as string)] as Resolved;
     }
   });
 
   // could be a Map<id, number> instead of tuple array for better performance?
   const resolved = parsed.filter(
-    ([_id, n]) => typeof n === "number"
-  ) as Resolved[];
-  console.log(resolved);
+    ([_id, n]) => typeof n === "number" || n === "X"
+  ) as (Resolved | Pending)[];
+  console.log("#resolved", resolved.length);
 
   const unknown = parsed.filter(
-    ([_id, n]) => typeof n === "string"
+    ([_id, n]) => typeof n === "string" && n !== "X"
   ) as Unknown[];
 
-  console.log(unknown);
+  console.log("#unknown", unknown.length);
 
   let i = 0;
 
   while (unknown.length !== 0) {
-    const [id, n] = unknown.shift() as [string, string];
-    console.log(`id: ${id}, n: ${n}`);
+    const [id, n] = unknown.shift() as [string, Operation];
+    // console.log(`id: ${id}, n: ${n}`);
+
     const [left, operand, right] = n.trim().split(" ") as [
       left: string,
       operand: Operand,
       right: string
     ];
-    console.log(`left: ${left}, operand: ${operand}, right: ${right}`);
+    // console.log(`left: ${left}, operand: ${operand}, right: ${right}`);
 
     const ln = resolved.find(([id]) => id === left);
     const rn = resolved.find(([id]) => id === right);
 
     if (ln && rn) {
-      if (operand == "+") {
-        resolved.push([id, ln[1] + rn[1]]);
-      } else if (operand === "-") {
-        resolved.push([id, ln[1] - rn[1]]);
-      } else if (operand === "*") {
-        resolved.push([id, ln[1] * rn[1]]);
+      // console.log(`ln: ${ln}`);
+      // console.log(`rn: ${rn}`);
+
+      const statement = `${ln[1]} ${operand} ${rn[1]}`;
+      if (typeof ln[1] === "string" || typeof rn[1] === "string") {
+        if (id === "root") {
+          console.log("@ROOT");
+          resolved.push([id, `${ln[1]} = ${rn[1]}`]);
+          break;
+        }
+        resolved.push([id, `(${statement})`]);
       } else {
-        resolved.push([id, ln[1] / rn[1]]);
+        resolved.push([id, eval(statement)]);
+        if (id === "root") {
+          console.log("BREAK @ ROOT, fully solved?");
+          break;
+        }
       }
-      // if (id === 'root') {
-      //   console.log('BREAK');
-      // }
     } else {
-      console.log('CONTINUE');
-      
+      if (unknown.length === 1 && id === HUMAN) {
+        break;
+      }
       unknown.push([id, n]);
     }
     i++;
   }
   console.log(`final i: ${i}`);
 
-  console.log(resolved);
+  const root = resolved.find(([id]) => id === "root");
+  console.log(root);
 
-  const root = resolved.find(([id]) => id === 'root')
-  console.log('root =', root)
+  if (!root) {
+    throw new Error(`Root not found`);
+  }
+  const answer = solve(root[1] as string);
+  console.log("answer:", answer);
+}
+
+function solve(e: Expression): number {
+  const [left, right] = arrangeExpression(e);
+
+  // console.log(`right=${right}`);
+  // console.log(`left=${left}`);
+
+  if (left === "X") {
+    return parseFloat(right);
+  }
+
+  // left = right
+  else if (left.includes("X")) {
+    // always true?
+    const [oL, operator] = left.split(" ", 2);
+    const oR = left.slice(left.indexOf(operator) + 1).trim();
+    console.log(`l: ${oL}, o: ${operator}, r: ${oR}`);
+
+    const [dRight, dR, dL] = [right, oR, oL].map(parseFloat);
+
+    // console.log(
+    //   `dRight: ${dRight}, dR: ${dR}, dL: ${dL}, operand: ${operator}`
+    // );
+
+    // return l.includes("X")
+    //   ? solve(`${l} = ${eval(`${dRight} ${o} ${dR}`)}`)
+    //   : solve(`${r} = ${eval(`${dRight} ${o} ${dR}`)}`);
+    if (operator === "+") {
+      return oL.includes("X")
+        ? solve(`${oL} = ${dRight - dR}`)
+        : solve(`${oR} = ${dRight - dL}`);
+      // return solve(`${r} = ${dRight - dL}`);
+    } else if (operator === "-") {
+      return oL.includes("X")
+        ? solve(`${oL} = ${dRight + dR}`)
+        : solve(`${oR} = ${dL - dRight}`);
+      // return solve(`${r} = ${dRight + dL}`);
+    } else if (operator === "*") {
+      return oL.includes("X")
+        ? solve(`${oL} = ${dRight / dR}`)
+        : solve(`${oR} = ${dRight / dL}`);
+      // return solve(`${r} = ${dRight / dL}`);
+    } else {
+      return oL.includes("X")
+        ? solve(`${oL} = ${dRight * dR}`)
+        : solve(`${oR} = ${dL / dRight}`);
+      // return solve(`${r} = ${dRight * dL}`);
+    }
+  }
+  return Number.POSITIVE_INFINITY;
+}
+
+function arrangeExpression(e: Expression): [unsolved: string, solved: string] {
+  const [left, right] = e.split("=").map((variable) => {
+    const trimmed = variable.trim();
+    return trimmed.startsWith("(") && trimmed.endsWith(")")
+      ? trimmed.slice(1, -1)
+      : trimmed;
+  });
+
+  if (left.startsWith("(")) {
+    const variableAndOperator = left
+      .split("")
+      .splice(left.lastIndexOf(")") + 1)
+      .join("")
+      .split(" ")
+      .reverse()
+      .join(" ");
+    const leftArranged =
+      variableAndOperator +
+      left
+        .split("")
+        .splice(0, left.lastIndexOf(")") + 1)
+        .join("");
+
+    const [l, o] = leftArranged.split(" ", 2);
+
+    if (o === "-") {
+      return [
+        [
+          `${parseFloat(l) * -1}`,
+          " +",
+          ...leftArranged.slice(leftArranged.indexOf(o) + 1),
+        ].join(""),
+        right,
+      ];
+    }
+
+    if (o === "/") {
+      return [
+        [
+          `${1 / parseFloat(l)}`,
+          " *",
+          ...leftArranged.slice(leftArranged.indexOf(o) + 1),
+        ].join(""),
+        right,
+      ];
+    }
+
+    return [leftArranged, right];
+  }
+
+  return left.includes("X") ? [left, right] : [right, left];
 }
 
 async function readInput(filename: string): Promise<Instruction[]> {
@@ -108,4 +230,4 @@ async function readInput(filename: string): Promise<Instruction[]> {
 })();
 
 // for tests
-export {};
+export { solve, arrangeExpression };
