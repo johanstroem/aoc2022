acc20db2e42412766cfedb70a1c04c2dce42c36e
JohanS <johan.strom@mentimeter.com>
2023-01-15 20:28:29 +0100

    day23 performance and cleanup

--- src/day23/index.ts
+++ src/day23/index.ts
@@ -2,33 +2,22 @@ import { REAL_INPUT, TEST_INPUT } from "../utils/globals";
 import { Map, Index, printMap } from "../utils";
 import createLineProcessor from "../utils/lineProcessor";
 
-// Similarities to day 14?
-
 const MOVES = ["N", "S", "W", "E"] as const;
 
 type Moves = typeof MOVES[number];
-// const DIRECTIONS = {
-//   NORTH: "NORTH",
-//   SOUTH: "SOUTH",
-//   WEST: "WEST",
-//   EAST: "EAST",
-// } as const;
 
 const DIRECTIONS = (set: Set<string>) =>
   ({
     NORTH: (e: string) => {
       const [row, col] = e.split(",").map((v) => parseInt(v));
-      // console.log(`row: ${row}, col: ${col}`);
       return !(
         set.has(`${row - 1},${col - 1}`) ||
         set.has(`${row - 1},${col}`) ||
         set.has(`${row - 1},${col + 1}`)
       );
     },
-
     SOUTH: (e: string) => {
       const [row, col] = e.split(",").map((v) => parseInt(v));
-      // console.log(`row: ${row}, col: ${col}`);
       return !(
         set.has(`${row + 1},${col - 1}`) ||
         set.has(`${row + 1},${col}`) ||
@@ -37,7 +26,6 @@ const DIRECTIONS = (set: Set<string>) =>
     },
     WEST: (e: string) => {
       const [row, col] = e.split(",").map((v) => parseInt(v));
-      // console.log(`row: ${row}, col: ${col}`);
       return !(
         set.has(`${row - 1},${col - 1}`) ||
         set.has(`${row},${col - 1}`) ||
@@ -46,7 +34,6 @@ const DIRECTIONS = (set: Set<string>) =>
     },
     EAST: (e: string) => {
       const [row, col] = e.split(",").map((v) => parseInt(v));
-      // console.log(`row: ${row}, col: ${col}`);
       return !(
         set.has(`${row - 1},${col + 1}`) ||
         set.has(`${row},${col + 1}`) ||
@@ -55,16 +42,88 @@ const DIRECTIONS = (set: Set<string>) =>
     },
   } as const);
 
-// const MOVE = {
-//   'NORTH': (e: string) => {
-//     const [row, col] = e.split(",").map((v) => parseInt(v));
-//     return [row -1,col].toString()
-//   },
-//   'SOUTH': (e: string) => {
-//     const [row, col] = e.split(",").map((v) => parseInt(v));
+async function countGroundTiles(filename = TEST_INPUT) {
+  const input: Map<"." | "#"> = await readInput(filename);
 
-//   }
-// }
+  let elves = new Set<string>(
+    input.flatMap((r, i) => {
+      return r
+        .map((e, j) => {
+          return e === "#" ? [i, j].toString() : null!;
+        })
+        .filter((e) => e !== null);
+    })
+  );
+
+  console.log("#Initial elves:", elves.size);
+
+  const order = [...MOVES];
+  let moved = true;
+  let i = 0;
+
+  while (moved) {
+    moved = false;
+    const directions = DIRECTIONS(elves);
+    const next: typeof elves = new Set();
+
+    for (let e of elves) {
+      const movableDirections = order
+        .map((dir) => {
+          if (dir === "N") {
+            return directions["NORTH"];
+          } else if (dir === "S") {
+            return directions["SOUTH"];
+          } else if (dir === "W") {
+            return directions["WEST"];
+          } else {
+            return directions["EAST"];
+          }
+        })
+        .map((direction) => direction(e));
+
+      if (movableDirections.every((d) => d)) {
+        next.add(e);
+      } else if (movableDirections.every((d) => !d)) {
+        next.add(e);
+      } else {
+        const moveDirection = order[movableDirections.indexOf(true)];
+        const moving = move(e, moveDirection);
+        if (next.delete(moving)) {
+          next.add(e);
+          next.add(move(moving, moveDirection)); // undo previous move coming from opposite direction
+        } else {
+          next.add(moving);
+          moved = true;
+        }
+      }
+    }
+
+    if (!moved) {
+      console.log(`No elves moved: [${i + 1}]`);
+      break;
+    }
+
+    elves = next;
+    if (++i === 10) {
+      console.log("After 10 rounds:");
+      printMap({
+        map: toMap(elves),
+      });
+      const [rMin, cMin, rMax, cMax] = getBoundingBox(elves);
+      console.log(`rMin: ${rMin}, cMin: ${cMin}, rMax: ${rMax}, cMax: ${cMax}`);
+      console.log("#elves:", elves.size);
+      console.log(
+        "#Ground tiles:",
+        (rMax - rMin + 1) * (cMax - cMin + 1) - elves.size
+      );
+    }
+    order.push(order.shift()!);
+  }
+
+  printMap({
+    map: toMap(elves),
+  });
+}
 
 function move(e: string, move: Moves) {
   const [row, col] = e.split(",").map((v) => parseInt(v));
@@ -86,219 +145,25 @@ function move(e: string, move: Moves) {
     }
   }
 }
-// type Directions = typeof DIRECTIONS[keyof typeof DIRECTIONS];
-// type Directions = keyof typeof DIRECTIONS;
-
-async function countGroundTiles(filename = REAL_INPUT) {
-  const input: Map<"." | "#"> = await readInput(filename);
-
-  // printMap({ map: input });
-  // console.log(
-  //   // "input",
-  //   // input,
-  //   "#rows:",
-  //   input.length,
-  //   "#cols:",
-  //   input[0].length
-  // );
-
-  // .filter((e): e is string => e !== null);
-  let elves = new Set<string>(
-    input.flatMap((r, i) => {
-      return r
-        .map((e, j) => {
-          return e === "#" ? [i, j].toString() : null!;
-        })
-        .filter((e) => e !== null);
-    })
-  );
-  // console.log("elves", elves, "#elves", elves.size);
-
-  // const [rMin, cMin, rMax, cMax] = getBoundingBox(elves);
-
-  // printMap({ map: toMap(elves) });
-
-  const ITERATIONS = 10000;
-
-  const order = [...MOVES];
-
-  for (let i = 0; i < ITERATIONS; i++) {
-    // Only works for first iteration?
-    // console.log("order", order);
-    // const direction = order.shift();
-    // order.push(direction!)
-
-    const directions = DIRECTIONS(elves);
-    // const moves: [
-    //   north: string[],
-    //   south: string[],
-    //   west: string[],
-    //   east: string[]
-    // ] = [[], [], [], []];
-    const moves: Record<Moves, string[]> = {
-      N: [],
-      S: [],
-      W: [],
-      E: [],
-    };
-
-    const next: typeof elves = new Set();
-
-    for (let e of elves) {
-      // const [row, col] = e.split(",").map((v) => parseInt(v));
-      // console.log(`row: ${row}, col: ${col}`);
-
-      // Do for all elves
-      const movableDirections = order
-        .map((dir, _i) => {
-          if (dir === "N") {
-            return directions["NORTH"];
-          } else if (dir === "S") {
-            return directions["SOUTH"];
-          } else if (dir === "W") {
-            return directions["WEST"];
-          } else {
-            return directions["EAST"];
-          }
-        })
-        // run for each elves' coordinates
-        .map((direction) => direction(e));
-
-      if (movableDirections.every((d) => d)) {
-        // Stay put
-        // console.log("STAY PUT");
-        next.add(e);
-        // continue
-      } else if (movableDirections.every((d) => d === false)) {
-        // cannot move?
-        // console.log("CANNOT MOVE");
-        // continue
-        next.add(e);
-      } else {
-        const moveDirection = order[movableDirections.indexOf(true)];
-        // console.log("moveDirection", moveDirection);
-        moves[moveDirection].push(e);
-      }
-    }
-
-    if (Object.keys(moves).every((dir) => moves[dir as Moves].length === 0)) {
-      console.log(`NO MORE MOVES: [${i}]`);
-      break;
-    }
-    // console.log("next1", next);
-
-    // console.log("moves", moves);
-
-    const northBound = new Set(moves["N"].map((e) => move(e, "N")));
-    const southBound = new Set(moves["S"].map((e) => move(e, "S")));
-    const westBound = new Set(moves["W"].map((e) => move(e, "W")));
-    const eastBound = new Set(moves["E"].map((e) => move(e, "E")));
-
-    // console.log("northBound", northBound);
-    // console.log("southBound", southBound);
-    // console.log("westBound", westBound);
-    // console.log("eastBound", eastBound);
-
-    for (const [dir, m] of Object.entries(moves)) {
-      // console.log("dir:", dir, "m:", m);
-      if (dir === "N") {
-        const rest = new Set([...southBound, ...westBound, ...eastBound]);
-        // console.log("rest", rest);
-        m.forEach((d) => {
-          rest.has(move(d, dir)) ? next.add(d) : next.add(move(d, dir));
-        });
-      } else if (dir === "S") {
-        const rest = new Set([...northBound, ...westBound, ...eastBound]);
-        m.forEach((d) => {
-          rest.has(move(d, dir)) ? next.add(d) : next.add(move(d, dir));
-        });
-      } else if (dir === "W") {
-        const rest = new Set([...northBound, ...southBound, ...eastBound]);
-        m.forEach((d) => {
-          rest.has(move(d, dir)) ? next.add(d) : next.add(move(d, dir));
-        });
-      } else if (dir === "E") {
-        const rest = new Set([...northBound, ...southBound, ...westBound]);
-        m.forEach((d) => {
-          rest.has(move(d, dir)) ? next.add(d) : next.add(move(d, dir));
-        });
-      } else {
-        throw new Error(`Invalid direction: ${dir}`);
-      }
-    }
-
-    // console.log("next2", next);
-    // const [rMin, cMin, rMax, cMax] = getBoundingBox(next);
-    elves = next;
-
-    // printMap({
-    //   map: toMap(elves),
-    //   // size: {
-    //   //   minRow: rMin,
-    //   //   rows: rMax,
-    //   //   minCol: cMin,
-    //   //   cols: cMax,
-    //   // },
-    // });
-
-    // Update order
-    order.push(order.shift()!);
-  }
-
-  // console.log(
-  //   `size: ${map.length} x ${map[0].length} = ${map.length * map[0].length}`
-  // );
-  const [rMin, cMin, rMax, cMax] = getBoundingBox(elves);
-
-  console.log("#elves:", elves.size);
-  console.log(
-    "#Ground tiles:",
-    (rMax - rMin + 1) * (cMax - cMin + 1) - elves.size
-  );
-  printMap({
-    map: toMap(elves),
-  });
-}
 
 function toMap(set: Set<string>): Map<"X" | "."> {
-  // const arr = Array.from(set).map(
-  //   (e) => e.split(",").map((e) => parseInt(e)) as unknown as Index
-  // );
-  // console.log("arr", arr, "#arr", arr.length);
-
   const [rMin, cMin, rMax, cMax] = getBoundingBox(set);
-
-  // console.log(`rMin: ${rMin}, rMax: ${rMax}`);
-  // console.log(`cMin: ${cMin}, cMax: ${cMax}`);
 
   const map = Array(rMax - rMin + 1)
     .fill(null)
-    // TODO: adjust for negative rows/cols to have zero based indexes
     .map((_) => Array(cMax - cMin + 1).fill("."));
 
   for (const e of set.values()) {
     const [row, col] = e.split(",").map((v) => parseInt(v));
     map[row - rMin][col - cMin] = "#";
-    // map[row - rMin][col - cMin] = [row - rMin, col - cMin].toString()
-    // map[row - rMin][col - cMin] = [row, col].toString()
   }
 
   return map;
 }
 
-// function getBoundingBox(
-//   elves: Set<string>
-// ): [rMin: number, cMin: number, rMax: number, cMax: number];
-// {
-//   return [0, 0, 1, 1];
-// }
-
 function getBoundingBox(
-  set: // Map<"#" | ".">
-  Set<string>
-  // Index[]
+  set: Set<string>
 ): [rMin: number, cMin: number, rMax: number, cMax: number] {
-  // TODO: adjust for negative rows/cols to have zero based indexes
   const arr = Array.from(set).map(
     (e) => e.split(",").map((e) => parseInt(e)) as unknown as Index
   );
@@ -309,7 +174,6 @@ function getBoundingBox(
   const cMin = Math.min(...cols);
   const rMax = Math.max(...rows);
   const cMax = Math.max(...cols);
-  console.log(`rMin: ${rMin}, cMin: ${cMin}, rMax: ${rMax}, cMax: ${cMax}`);
 
   return [rMin, cMin, rMax, cMax];
 }
